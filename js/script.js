df7eab0c0e091498b5ae190b2a955f2d70693ca0
Eboscaatto <e_boscatto@hotmail.com>
2024-02-22 15:22:42 -0300

    Criada a pasta js e o arquivo script.js

--- js/script.js
+++ js/script.js
@@ -1,8 +1,16 @@
 function validarFormulario() {
-    alert("Dados enviados com sucesso!")    
+    let nome = document.getElementById('nome').value
+    let email = document.getElementById('email').value
+
+    if (nome && email !=0) {
+        swal("Parabéns!","Dados enviados com sucesso.");                      
+    } else {
+        swal("Ops, Erro!","Todos os campos devem ser preenchidos.");      
+    }       
 }
 
 function limparFormulario() {    
     document.getElementById('nome').value="";
     document.getElementById('email').value="";
-}
+} 
+
